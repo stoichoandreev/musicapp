038bb25c7cf318975e846efa5b4816637996aa73
Silviata 77 <silviata77@gmail.com>
2018-10-09 01:16:21 +0100

    Added working step definitions for artist search feature test

--- app/src/androidTest/assets/features/artist_search.feature
+++ app/src/androidTest/assets/features/artist_search.feature
@@ -1,18 +1,7 @@
 Feature: Artist Search
 
-#  @Smoke
-#  Scenario: Show Artist Search Results
-#    Given I start the application
-#    When  I click search icon
-#    Then I expect to see the following artist search results
-
   @smoke
   Scenario Outline: Show Artist Search Results
-#    Given the following remote artist results exist
-#      | mbId                                      | name |
-#      | bfcc6d75-a6a5-4bc6-8282-47aec8531818      | Cher |
-#      | 2d499150-1c42-4ffb-a90c-1cc635519d33      | Cheryl Cole |
-#      | 48fbfb0b-92ee-45eb-99c2-0bde4c05962e      | Cher Lloyd |
     When I start the application
     And  I click search icon
     And  I enter search query <query>
--- app/src/androidTest/java/com/sniper/music/cucumber/steps/ArtistSearchSteps.java
+++ app/src/androidTest/java/com/sniper/music/cucumber/steps/ArtistSearchSteps.java
@@ -7,13 +7,13 @@ import com.sniper.music.espresso.hometests.robot.HomeScreenRobot;
 import com.sniper.music.home.HomeActivity;
 import com.sniper.music.utils.rules.FinishOpenActivitiesRule;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 
 import java.util.Map;
 
 import cucumber.api.DataTable;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -23,29 +23,27 @@ public class ArtistSearchSteps {
 
     private static HomeConfigurator homeConfigurator = new HomeConfigurator();
 
-//    @Rule
-//    public ActivityTestRule<HomeActivity> activityRule = new ActivityTestRule<>(HomeActivity.class);
+    private ActivityTestRule<HomeActivity> activityRule = new ActivityTestRule<>(HomeActivity.class);
 
     @Rule
     public FinishOpenActivitiesRule finishOpenActivitiesRule = new FinishOpenActivitiesRule();
 
     private HomeScreenRobot robot = new HomeScreenRobot();
 
-    @BeforeClass
-    public static void beforeClass() {
+    @Before
+    public void setup() {
         homeConfigurator.setup();
         homeConfigurator.setSearchResponse();
     }
 
-    @AfterClass
-    public static void afterClass() {
+    @After
+    public void tearDown() {
         homeConfigurator.tearDown();
     }
 
     @Given("^I start the application$")
     public void I_start_app() {
-        robot.launchHomeScreen(new ActivityTestRule<>(HomeActivity.class));
-//        robot.launchHomeScreen(activityRule);
+        robot.launchHomeScreen(activityRule);
     }
 
     @And("^I click search icon")
@@ -76,49 +74,6 @@ public class ArtistSearchSteps {
         String artistName = row.get("name");
         robot.isArtistNameDisplayed(Integer.parseInt(position), artistName);
     }
-
-
-
-//
-//    @Given("^the following remote movie posters exist$")
-//    public void the_following_remote_movie_poster_exist(final DataTable dataTable) {
-//        extractPostersFromDataTable(dataTable);
-//    }
-//
-//    private void extractPostersFromDataTable(DataTable dataTable) {
-//        ConfigurableBackend configurableBackend = getConfigurableBackend();
-//
-//        for (final Map<String, String> row : dataTable.asMaps(String.class, String.class)) {
-//            Long movieId = Long.valueOf(row.get("movieId"));
-//            String posterPath = row.get("posterPath");
-//
-//            configurableBackend.addToPopularStream(createApiMoviePoster(movieId, posterPath));
-//        }
-//    }
-//
-//    private ApiMoviePoster createApiMoviePoster(long movieId, String posterPath) {
-//        return new ApiMoviePoster(movieId, posterPath);
-//    }
-//
-//    @When("^I select the poster at position (\\d+)$")
-//    public void I_select_a_poster_at(final int position) {
-//        popularMoviesRobot.selectPosterAtPosition(position);
-//    }
-//
-//    @When("^I expect to see the following movie posters$")
-//    public void I_expect_to_see_movie_posters(final DataTable dataTable) {
-//        for (Map<String, String> row : dataTable.asMaps(String.class, String.class)) {
-//            int position = Integer.valueOf(row.get("position"));
-//            String posterPath = row.get("posterPath");
-//
-//            popularMoviesRobot.checkPosterWithPathIsDisplayedAtPosition(position, posterPath);
-//        }
-//    }
-//
-//    private ConfigurableBackend getConfigurableBackend() {
-//        MovieApplication movieApplication = (MovieApplication) InstrumentationRegistry.getTargetContext().getApplicationContext();
-//        return (ConfigurableBackend) ((TestPopularMoviesComponent) movieApplication.getPopularMoviesComponent()).backend();
-//    }
 
 
 }
