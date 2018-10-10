aa5ecddd8dcd85f9ea79a5720b3b1ce109c8792e
Silviata 77 <silviata77@gmail.com>
2018-10-10 20:44:54 +0100

    Adding more E2E tests. Adding activity finisher for the E2E tests. Adding more cucumber tags

--- app/src/androidTest/java/com/sniper/music/espresso/detailstests/robot/DetailsScreenRobot.java
+++ app/src/androidTest/java/com/sniper/music/espresso/detailstests/robot/DetailsScreenRobot.java
@@ -21,11 +21,27 @@ public class DetailsScreenRobot {
         return this;
     }
 
+    public DetailsScreenRobot isArtistTitleWithExpectedInformation(String expectedTitle) {
+        onView(withId(R.id.details_title_view)).check(matches(withText(expectedTitle)));
+        return this;
+    }
+
+    public DetailsScreenRobot isArtistListenersWithExpectedInformation(String expectedListenersInfo) {
+        onView(withId(R.id.details_listeners_view)).check(matches(withText(expectedListenersInfo)));
+        return this;
+    }
+
+    public DetailsScreenRobot isArtistPlayedWithExpectedInformation(String expectedPlayedInfo) {
+        onView(withId(R.id.details_play_count_view)).check(matches(withText(expectedPlayedInfo)));
+        return this;
+    }
+
     public DetailsScreenRobot areAllViewsWithExpectedInformation() {
+        isArtistTitleWithExpectedInformation("Cher");
+        isArtistListenersWithExpectedInformation("Artist Listeners : 1076938");
+        isArtistPlayedWithExpectedInformation("Was Played : 14952381");
+
         onView(withId(R.id.details_image_view)).check(matches(withContentDescription("https://lastfm-img2.akamaized.net/i/u/174s/879a88760860cc472d826ca4e7fc5ad6.png")));
-        onView(withId(R.id.details_title_view)).check(matches(withText("Cher")));
-        onView(withId(R.id.details_listeners_view)).check(matches(withText("Artist Listeners : 1076938")));
-        onView(withId(R.id.details_play_count_view)).check(matches(withText("Was Played : 14952381")));
         onView(withId(R.id.details_url_view)).check(matches(withText("https://www.last.fm/music/Cher")));
         onView(withId(R.id.details_description_view)).check(matches(withText("Cher (born Cherilyn Sarkisian; May 20, 1946) is an Oscar- and Grammy-winning American singer and actress. A major figure for over five decades in the world of popular culture, she is often referred to as the Goddess of Pop for having first brought the sense of female autonomy and self-actualization into the entertainment industry.She is known for her distinctive contralto and for having worked extensively across media, as well as for continuously reinventing both her music and image, the latter of which has been known to induce controversy.")));
         return this;
